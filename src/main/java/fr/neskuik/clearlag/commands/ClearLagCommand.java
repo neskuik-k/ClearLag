67d590d62b7a9a163f607b5411bedb27fc243d69
Neskuik_k <ythoneybeee@gmail.com>
2025-02-10 16:54:26 +0100

    refractor: added reload subcommand

--- src/main/java/fr/neskuik/clearlag/commands/ClearLagCommand.java
+++ src/main/java/fr/neskuik/clearlag/commands/ClearLagCommand.java
@@ -4,6 +4,7 @@ import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Default;
+import co.aikar.commands.annotation.Subcommand;
 import fr.neskuik.clearlag.ClearLag;
 import fr.neskuik.clearlag.listener.EntityUtils;
 import org.bukkit.Bukkit;
@@ -23,5 +24,11 @@ public class ClearLagCommand extends BaseCommand {
         int removed = EntityUtils.clearEntities(plugin);
         sender.sendMessage("§6§lSKYMC §f§l• §7Suppression de " + removed + " entités §aeffectué(e)§7 !");
         Bukkit.broadcastMessage("§7[§6⚠§7] §fSuppresion des items au sol effectué.");
+    }
+
+    @Subcommand("reload")
+    public void onReload(CommandSender sender) {
+        plugin.reloadClearLagConfig();
+        sender.sendMessage("§6§lSKYMC §f§l• §7Configuration rechargée.");
     }
 }
